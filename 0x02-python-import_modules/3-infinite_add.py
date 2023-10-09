696084d0ecea5817d3955a213822ae8d75102303
moochies <mutukukelvin99@gmail.com>
2023-10-09 02:05:17 -0700

    updated

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+
+    for item in range(1, len(sys.argv)):
+        total = total + int(sys.argv[item])
+    print("{}".format(total))
