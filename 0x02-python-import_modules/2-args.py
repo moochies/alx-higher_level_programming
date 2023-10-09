fbf43121d51b188874569a9ac465aa9fad1cbc96
moochies <mutukukelvin99@gmail.com>
2023-10-09 02:03:46 -0700

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
 
     args_count = len(sys.argv)
 
     if args_count > 1:
-        print("{} argument{}:".format(args_count - 1, 's' if args_count - 1 != 1 else ''))
+        print("{} argument:".format(args_count - 1) if args_count == 2 else
+              "{} arguments:".format(args_count - 1))
         for arg in range(1, args_count):
             print("{}: {}".format(arg, sys.argv[arg]))
     else:
