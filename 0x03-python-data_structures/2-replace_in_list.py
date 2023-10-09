e56d58af2fb7ea6524a6c32d9e45c47bf5906fed
moochies <mutukukelvin99@gmail.com>
2023-10-09 05:30:01 -0700

    updated

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+
+    list_len = len(my_list) - 1
+    if idx > list_len:
+        return my_list
+
+    my_list[idx] = element
+    return my_list
