b02b121707626fafdb82e7df45ee94915f7d709f
moochies <mutukukelvin99@gmail.com>
2023-10-30 08:09:02 -0700

    updated files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    Rectangle class
+"""
+
+
+class Rectangle:
+    """Defines Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3.8
+
+
+"""
+    class Rectangle
+"""
+
+
+class Rectangle:
+    """
+        Defines REctangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def perimeter(self):
+        if (self.__width) == 0 or (self.__height) == 0:
+            return (0)
+
+        return (self.__height + self.__width) * 2
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def __str__(self):
+        """Sets the print behavior of the Rectangle object."""
+        rectangle = ""
+
+        if self.__width > 0 and self.__height > 0:
+            for y in range(self.__height):
+                rectangle += '#' * self.__width + '\n'
+
+        return rectangle[:-1]
+
+    def __repr__(self):
+        """Return a string representation"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
