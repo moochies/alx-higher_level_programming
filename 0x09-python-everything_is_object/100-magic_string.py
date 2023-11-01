88af3027faf5a93588412e3273ebe9387296205b
moochies <mutukukelvin99@gmail.com>
2023-11-01 02:11:12 -0700

    updated files

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.8
+def magic_string(string=[]):
+    string.append("BestSchool")
+    return ", ".join(string)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""locked class"""
+
+
+class LockedClass:
+    """Locked class"""
+    __slots__ = ['first_name']
