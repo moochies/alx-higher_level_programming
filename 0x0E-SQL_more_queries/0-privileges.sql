c6f4156477a2c1a481e9a065515ad5abc48694ea
moochies <mutukukelvin99@gmail.com>
2023-12-14 03:46:51 -0800

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,8 @@
 -- script that lists all privileges of the MYSQL users
 -- user_0d_1 and user_0d_2 on the server(in localhost).
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
 
 -- grants for the other user.
 
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
