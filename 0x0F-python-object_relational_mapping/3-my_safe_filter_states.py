ed2cd3f78de873b38981ce3b28fe6cd7e443dd96
moochies <mutukukelvin99@gmail.com>
2024-01-14 23:47:51 -0800

    updated file

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
+"""python script that connects to MySQL db and
+fetches data. Data is filtered by selecting items with the
+name  passed as argument . Uses a method that circumvents sql
+injection"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+args = sys.argv
+
+
+def list_():
+    """function fetches filtered data and prints it.
+        print all items with name passed as argument
+    """
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("""SELECT * FROM states WHERE BINARY name = %s
+     ORDER BY id ASC""", ["{}".format(args[4])])
+    results = cur.fetchall()
+
+    for state in results:
+        print(state)
+
     cur.close()
     db.close()
+
+
+if __name__ == '__main__':
+    list_()
